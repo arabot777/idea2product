4492bc59b47d0c9baa79dc10027d07d17ad10679
longgui0318 <>
2025-06-13 17:18:49 +0800

    rest lib/db/migrations

--- lib/db/migrations/0001_yellow_marvel_zombies.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "tasks" ADD COLUMN "current_request_amount" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
-ALTER TABLE "tasks" ADD COLUMN "external_id" text;--> statement-breakpoint
-ALTER TABLE "tasks" ADD COLUMN "external_metric_event_id" text;
